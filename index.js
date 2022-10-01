f34c4a30c6cf32c3b94fbf37e0550523fe412b80
TheSunshade <82448383+thesunshade@users.noreply.github.com>
2022-10-01 14:24:24 +0530

    start

---
+++ index.js
@@ -0,0 +1,80 @@
+import { buildSutta } from "./buildSutta.js";
+// import { JsDiff } from "./diff.js";
+
+const form1 = document.getElementById("sutta-one-form");
+const suttaOneCitation = document.getElementById("sutta-one-citation");
+const suttaOneContent = document.getElementById("sutta-one-content");
+
+const form2 = document.getElementById("sutta-two-form");
+const suttaTwoCitation = document.getElementById("sutta-two-citation");
+const suttaTwoContent = document.getElementById("sutta-two-content");
+
+form1.addEventListener("submit", e => {
+  e.preventDefault();
+  if (suttaOneCitation.value) {
+    buildSutta(suttaOneCitation.value, suttaOneContent);
+  }
+});
+form2.addEventListener("submit", e => {
+  e.preventDefault();
+  if (suttaTwoCitation.value) {
+    buildSutta(suttaTwoCitation.value, suttaTwoContent);
+  }
+});
+
+/* -------ORIGINAL------------*/
+var a = suttaOneContent;
+var b = suttaTwoContent;
+var result = document.getElementById("comparison");
+
+function changed() {
+  var diff = JsDiff[window.diffType](a.textContent, b.textContent);
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < diff.length; i++) {
+    if (diff[i].added && diff[i + 1] && diff[i + 1].removed) {
+      var swap = diff[i];
+      diff[i] = diff[i + 1];
+      diff[i + 1] = swap;
+    }
+
+    var node;
+    if (diff[i].removed) {
+      node = document.createElement("del");
+      node.appendChild(document.createTextNode(diff[i].value));
+    } else if (diff[i].added) {
+      node = document.createElement("ins");
+      node.appendChild(document.createTextNode(diff[i].value));
+    } else {
+      node = document.createTextNode(diff[i].value);
+    }
+    fragment.appendChild(node);
+  }
+
+  result.textContent = "";
+  result.appendChild(fragment);
+}
+
+window.onload = function () {
+  onDiffTypeChange(document.querySelector('#settings [name="diff_type"]:checked'));
+  changed();
+};
+
+a.onchange = b.onchange = changed;
+
+if ("oninput" in a) {
+  a.oninput = b.oninput = changed;
+} else {
+  a.onkeyup = b.onkeyup = changed;
+}
+
+function onDiffTypeChange(radio) {
+  window.diffType = radio.value;
+}
+
+var radio = document.getElementsByName("diff_type");
+for (var i = 0; i < radio.length; i++) {
+  radio[i].onchange = function (e) {
+    onDiffTypeChange(e.target);
+    changed();
+  };
+}
